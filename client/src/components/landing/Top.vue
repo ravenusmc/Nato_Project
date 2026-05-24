6ea48b71565bed76573ad982ea14e5c61fc51c29
ravenusmc <mcuddy77@gmail.com>
2026-05-24 15:10:01 -0400

    working on top half of landing page

--- client/src/components/landing/Top.vue
+++ client/src/components/landing/Top.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <div>
-      
-    </div>
+      <div class="picture-div">
 
+      </div>
+      <div>
+        
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +17,20 @@ export default {
   name: 'Top',
 }
 </script>
+
+<style scoped>
+
+/* .picture-div {
+  width: 100%;
+  height: 750px;
+  border-radius: 10px;
+  background: url("../../assets/images/div_landing_photo.jpg") center/cover no-repeat;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.7);
+  display: flex; 
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 5%;
+} */
+
+</style>
