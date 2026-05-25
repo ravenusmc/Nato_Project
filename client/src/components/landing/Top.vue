0b71fbcb78bd103bae46c0c74d4c5dbce97e51a2
ravenusmc <mcuddy77@gmail.com>
2026-05-25 14:19:25 -0400

    minor change

--- client/src/components/landing/Top.vue
+++ client/src/components/landing/Top.vue
@@ -24,7 +24,7 @@ export default {
   width: 100%;
   height: 750px;
   border-radius: 10px;
-  background: url("../../assets/images/div_landing_photo.jpg") center/cover no-repeat;
+  background: url("../../assets/images/NATO_Woman.jpg") center/cover no-repeat;
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.7);
   display: flex; 
   flex-direction: column;
