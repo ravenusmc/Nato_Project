38613aed275aca264500d87a8e851ab121174e0c
ravenusmc <mcuddy77@gmail.com>
2026-06-03 16:53:15 -0400

    worked on bottom of landing page

--- client/src/components/landing/Bottom.vue
+++ client/src/components/landing/Bottom.vue
@@ -2,12 +2,15 @@
   <div>
     <div class="bottom-div">
       <div class="picture-div">
+        <div class="picture-inner-div">
+          <p>The Study</p>
+        </div>
       </div> 
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 
 .bottom-div {
   display: flex;
@@ -16,14 +19,29 @@
   align-items: center;
 }
 
-.picture-div { 
+.picture-div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
   background: url("../../assets/images/transport_plane.jpg") center/cover no-repeat;
   box-shadow: 0 4px 20px rgba(0, 0, 0, 0.7);
-  align-self: stretch;
-  margin: 2%;
   height: 500px;
-  border-radius: 100%;
-  margin: 5% 10%;
+  border-radius: 16px;
+  margin: 5% auto;
+}
+
+.picture-inner-div {
+  border: 1.5px solid rgba(255, 255, 255, 0.4);
+  background: rgba(0, 0, 0, 0.6);
+  padding: 30px;
+  border-radius: 12px;
+}
+
+p {
+  font-size: 50px;
+  color: white;
 }
 
 </style>
