df1f4102b3f0595220ef95392974592bc66b6289
ravenusmc <mcuddy77@gmail.com>
2026-06-07 13:00:52 -0400

    started work on form for user to enter year

--- client/src/views/DataView.vue
+++ client/src/views/DataView.vue
@@ -1,14 +1,55 @@
 <template>
   <div>
+    <div class="header-area">
+      <h3>The Data</h3>
+    </div>
+    <div class="form-area">
+      <form @submit.prevent="handleSubmit">
+        <div>
+          <label for="year">Year:</label>
+          <input 
+            id="username" 
+            type="number" 
+            v-model="year" 
+            placeholder="Enter Year:"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    <div class="world-map-area">
+
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      year: '',
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.year)
+    }
+  }
+}
 </script>
 
 <style scoped>
 
-p {
-  margin-top: 500px;
+.header-area {
+  display: grid;
+  place-items: center;
+  margin-top: 4%;
 }
+
+h3 {
+  text-transform: uppercase;
+  justify-content: center;
+  font-size: 2rem;
+}
+
 </style>
