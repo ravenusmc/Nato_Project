4a82fe2ff9a579cc1f8c646cb63fd396bc240bad
ravenusmc <mcuddy77@gmail.com>
2026-06-12 21:28:23 -0400

    minor change

---
+++ client/src/components/forms/Form.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="form-area">
+      <form @submit.prevent="handleSubmit">
+        <div>
+          <label for="year">Year:</label>
+          <input 
+            id="username" 
+            type="number" 
+            v-model="year" 
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
--- client/src/views/DataView.vue
+++ client/src/views/DataView.vue
@@ -3,19 +3,7 @@
     <div class="header-area">
       <h3>The Data</h3>
     </div>
-    <div class="form-area">
-      <form @submit.prevent="handleSubmit">
-        <div>
-          <label for="year">Year:</label>
-          <input 
-            id="username" 
-            type="number" 
-            v-model="year" 
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+
 
   </div>
 </template>
