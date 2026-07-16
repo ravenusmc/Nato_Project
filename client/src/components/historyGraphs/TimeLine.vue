ffb6ae29e1073643f65c8c42342c71a8b2ae3477
ravenusmc <mcuddy77@gmail.com>
2026-07-16 17:14:20 -0400

    setting up timeline component

--- client/src/components/historyGraphs/TimeLine.vue
+++ client/src/components/historyGraphs/TimeLine.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-      timeline!
+      <div ref="TimeLine"></div>
   </div>
 </template>
 
 <script>
+import * as d3 from "d3";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "TimeLine",
+  computed: {
+    ...mapGetters("datapage", ["NatoTimeLineData"]),
+  },
+}
 
 </script>
 
