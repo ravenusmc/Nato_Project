6a5a2c2690d5e0684015573199af3fed831d22dc
ravenusmc <mcuddy77@gmail.com>
2026-07-23 16:36:40 -0400

    working on building economies graph

--- client/src/components/EconomicGraphs/Economies.vue
+++ client/src/components/EconomicGraphs/Economies.vue
@@ -10,6 +10,16 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "EconomiesBarChart",
+  computed: {
+    ...mapGetters("datapage", ["NatoCountriesEconomies"]),
+  },
+  methods: {
+    buildEconomiesChart() {
+      const margin = { top: 50, right: 30, bottom: 50, left: 70 };
+      const width = 460 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
+    }
+  }
 }
 
 </script>
