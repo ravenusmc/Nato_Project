6c8ca97565c5db19801c84d143a75dea5e5671a1
ravenusmc <mcuddy77@gmail.com>
2026-07-24 16:10:51 -0400

    minor change

--- client/src/components/EconomicGraphs/Economies.vue
+++ client/src/components/EconomicGraphs/Economies.vue
@@ -18,6 +18,14 @@ export default {
       const margin = { top: 50, right: 30, bottom: 50, left: 70 };
       const width = 460 - margin.left - margin.right;
       const height = 400 - margin.top - margin.bottom;
+
+      const svg = d3
+        .select(this.$refs.economies)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
     }
   }
 }
