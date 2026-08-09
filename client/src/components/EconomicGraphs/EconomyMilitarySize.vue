34fcc01b01c82784c51dab18d28b387b87a45d63
ravenusmc <mcuddy77@gmail.com>
2026-08-09 12:25:37 -0400

    added more to military economy size graph on client side

--- client/src/components/EconomicGraphs/EconomyMilitarySize.vue
+++ client/src/components/EconomicGraphs/EconomyMilitarySize.vue
@@ -16,6 +16,45 @@ export default {
   },
     methods: {
     buildEconomyMilitarySizeGraph() {
+
+      const margin = { top: 50, right: 30, bottom: 50, left: 70 };
+      const width = 460 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
+      
+      const svg = d3
+        .select(this.$refs.EconomyMilitarySize)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+      
+      // X axis
+      const x = d3
+        .scaleBand()
+        .range([0, width])
+        .domain(this.EconomyMilitarySize.map((d) => d[0]))
+        .padding(0.2);
+      
+      // Draw the x-axis, then rotate its labels 45 degrees so long state names don't overlap
+      svg
+        .append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "rotate(45)")
+        .attr("text-anchor", "start")
+        .attr("dx", "0.5em")
+        .attr("dy", "0.5em");
+      
+      // Y axis
+      const y = d3
+        .scaleLinear()
+        .domain([0, d3.max(this.EconomyMilitarySize, (d) => d[1])])
+        .range([height, 0]);
+      svg.append("g").call(d3.axisLeft(y));
+      
+
     }
   },
   mounted() {
